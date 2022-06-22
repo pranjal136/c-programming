a7bbfec53464cf2ac5a4b278c4f9f2ef7d04b7c1
pranjal136 <107926588+pranjal136@users.noreply.github.com>
2022-06-22 19:23:59 +0530

    using float function on operators

---
+++ operator2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	/* code*/
+	int a;
+	float b;
+	a = 34;
+	b = 6.5;
+	printf("a + b = %f\n", a+b);
+	printf("a - b = %f\n", a-b);
+	printf("a * b = %f\n", a*b);
+	printf("a / b = %f\n", a/b);
+	
+	
+	
+	return 0;
+}

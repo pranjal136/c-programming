b34df5d669a77939ec5a4faa6b623daa24d26f93
pranjal136 <107926588+pranjal136@users.noreply.github.com>
2022-06-22 18:41:12 +0530

    find size of int

---
+++ size1.c
@@ -0,0 +1,12 @@
+// size of integer
+
+// url -> https://ide.geeksforgeeks.org/b326eba6-42e2-4d15-90c0-aeb39201a244
+
+
+#include <stdio.h>
+
+int main() 
+{
+	printf("%lu",sizeof(int));
+	return 0;
+}

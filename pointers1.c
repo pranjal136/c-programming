6c22eaa917f777d5efd0a237b3d7c3379ce9ff29
pranjal136 <107926588+pranjal136@users.noreply.github.com>
2022-06-30 19:40:11 +0530

    pointer with function and putting the address

---
+++ pointers1.c
@@ -0,0 +1,37 @@
+example 1:-
+#include<stdio.h>
+
+void ptrfunc(int *ptr)
+{
+	*ptr=8;
+}
+
+int main()
+{
+	int x=5;
+
+	printf("x=%d\n",x );
+	printf(&x);
+	printf("x=%d",x );
+	return 0;
+}
+
+example 2:-
+  #include<stdio.h>
+
+
+int main()
+{	
+	printf("Pointer Basics\n");
+	int a =76;
+	int *ptra=&a
+	int *ptr2= NULL;
+
+	printf("The Address of pointer is %p\n", &ptra );
+	printf("The Address of a is %p\n," &a);
+	printf("The Address of a is %p\n",ptra );
+	printf("The Address of some garbage is %p\n",ptr2);
+	printf("The Value of a is %d\n", *ptra );
+	printf("The Value of a is %d\n", *a );
+	return 0;
+}

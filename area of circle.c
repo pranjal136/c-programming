c41844795b9b4b04ef0f828265dd71a06f155e20
pranjal136 <107926588+pranjal136@users.noreply.github.com>
2022-07-23 21:52:42 +0530

    Update area of circle.c

--- area of circle.c
+++ area of circle.c
@@ -21,13 +21,13 @@ int main()
     printf("Enter the value of x1\n");
     scanf("%d", &x1);
 
-    printf("Enter the value of x1\n");
+    printf("Enter the value of y1\n");
     scanf("%d", &y1);
 
-    printf("Enter the value of x1\n");
+    printf("Enter the value of x2\n");
     scanf("%d", &x2);
 
-    printf("Enter the value of x1\n");
+    printf("Enter the value of y2\n");
     scanf("%d", &y2);
     dst = areaOfCircle(x1, y1, x2, y2, Edistance);
     printf("The distance between these points is %.2f\n", dst);
